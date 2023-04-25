0e93ad0401721d7904982b5c91fb1b75c07721e9
Pietromuresu <muresupietro66@gmail.com>
2023-04-25 23:26:15 +0200

    Comments

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
+// import vue
 const {createApp} = Vue;
 
+// create app and mount on vue
 createApp({
+  // create data 
   data(){
+    // create a return obj for data
     return{
       ciao: 'ciao',
       url: 'https://api.chucknorris.io/jokes/random',
@@ -12,13 +16,18 @@ createApp({
     }
   },
   methods: {
+    // create a method for use the api 
     getChuckNorris(){
+      // call the api 
       axios.get(this.url)
         .then(res => {
+          // valuing variables declared before 
           console.log(res.data)
           this.id = res.data.id
           this.text = res.data.value
+          // TODO:use the api link to the img 
           this.icon = 'img/Chuck_Norris_May_2015.jpeg'
+          // set a delay for loading end (not so useful in this case but is just for practice)
           setTimeout(()=>{
             this.loading = false
           }, 1000)
@@ -26,6 +35,7 @@ createApp({
     }
   },
   mounted(){
+    // evocate the function when page is mounted
     this.getChuckNorris()
 
   }
